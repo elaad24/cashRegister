c8c6452d24c4df51c487ed88805cd646a3b57ba4
unknown <elaad2408@gmail.com>
2021-08-09 23:04:37 +0300

    temp data befor server, database  data

---
+++ baseData.js
@@ -0,0 +1,21 @@
+export default [
+  {
+    name: "x report",
+    "what it does": "make  report  of the CASH that in the register ",
+    address: "./subjects/XReport",
+    color: "green",
+  },
+  {
+    name: "z report ",
+    "what it does":
+      "make  report  of the all  CASH that in the register  and cradits and tips ",
+    address: "./subjects/ZReport",
+    color: "teal",
+  },
+  {
+    name: "store  ",
+    "what it does": "open the store page  ",
+    address: "./subjects/Store",
+    color: "rgb(216, 62, 62)",
+  },
+];
---
+++ storeData.js
@@ -0,0 +1,10 @@
+export default [
+  { name: "pepsi", price: 19, color: "blue" },
+  { name: "cole", price: 21, color: "red" },
+  { name: "sprite", price: 18, color: "green" },
+  { name: "water", price: 10, color: "lightblue" },
+  { name: "sode", price: 15, color: "darkorange" },
+  { name: "Milkshake", price: 20, color: "lightslategrey" },
+  { name: "Coffee", price: 16, color: "tan" },
+  { name: "Wine", price: 13, color: "indianred" },
+];
