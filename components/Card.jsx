0fe10baff8440f12547bfa43cd38fdb48e070552
unknown <elaad2408@gmail.com>
2021-10-03 00:30:52 +0300

    add more data to pass to card

--- components/Card.jsx
+++ components/Card.jsx
@@ -3,12 +3,12 @@ import styles from "../styles/Home.module.css";
 
 // this callback should be a function that add the price to the total prive of order
 
-const Card = ({ name, color, price, callback }) => {
+const Card = ({ name, color, price, item_id, item_group, callback }) => {
   return (
     <button
       className={styles.card}
       style={{ backgroundColor: color }}
-      onClick={() => callback({ name, price })}
+      onClick={() => callback({ name, price, item_id, item_group })}
     >
       {name} : {price}
     </button>
