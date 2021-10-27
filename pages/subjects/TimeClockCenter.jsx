30dcbfb1621226cc79a48ce5ab1af4f1201958c2
unknown <elaad2408@gmail.com>
2021-10-27 15:16:19 +0300

    fixed bug in the table and data feching and add the button without  functionlty

--- pages/subjects/TimeClockCenter.jsx
+++ pages/subjects/TimeClockCenter.jsx
@@ -13,7 +13,7 @@ const TimeClockCenter = (props) => {
         <Link href={`/`}>
           <a className="m-4 btn btn-primary">home</a>
         </Link>
-        <h2 className={styles.title}>{"users center"}</h2>
+        <h2 className={styles.title}>{"time clock center"}</h2>
 
         <div>
           <button
@@ -42,11 +42,34 @@ const TimeClockCenter = (props) => {
                 <tr>
                   <td>{data.id}</td>
                   <td>{data.user_id}</td>
-                  <td>{data.start}</td>
-                  <td>{data.finish}</td>
-                  <td>{(data.completed = 1 ? "true" : "false")}</td>
+                  <td>
+                    {new Intl.DateTimeFormat("en-GB", {
+                      dateStyle: "short",
+                      timeStyle: "medium",
+                    }).format(data.start)}
+                  </td>
+                  <td>
+                    {new Intl.DateTimeFormat("en-GB", {
+                      dateStyle: "short",
+                      timeStyle: "medium",
+                    }).format(data.finish)}
+                  </td>
+                  <td>{data.completed == "1" ? "true" : "false"}</td>
                   <td>{data.duration.slice(0, 8)}</td>
-                  <td className="d-flex justify-content-center gap-4"></td>
+                  <td className="d-flex justify-content-center gap-4">
+                    <button
+                      className="btn btn-warning "
+                      /* onClick={() => openModal({}, "edit")} */
+                    >
+                      {"edit shift"}
+                    </button>
+                    <button
+                      className="btn btn-danger "
+                      /* onClick={() => openModal({}, "delete")} */
+                    >
+                      {"delete shift"}
+                    </button>
+                  </td>
                 </tr>
               );
             })}
@@ -64,14 +87,14 @@ export async function getStaticProps(context) {
 
   const results = await handler(mysql, qry);
 
-  console.log(results);
+  /* multply the start and finish times 1000 so it will be in miliseconds and not seconds  */
   return {
     props: {
       allData: results.map((data) => ({
         id: data.id,
         user_id: data.user_id,
-        start: data.start,
-        finish: data.finish,
+        start: data.start * 1e3,
+        finish: data.finish * 1e3,
         completed: data.completed,
         duration: data.duration,
       })),
