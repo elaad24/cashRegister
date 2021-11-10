2de5361e384e0f60563c3f4981a503ad6cbc51ab
unknown <elaad2408@gmail.com>
2021-11-10 17:06:14 +0200

    finsh stats front and back

--- components/StatsCard.jsx
+++ components/StatsCard.jsx
@@ -1,27 +1,45 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const StatsCard = ({ header, prefix, data, suffix, cardType }) => {
+const StatsCard = ({ header, prefix = "", data, suffix = "", cardType }) => {
   return (
-    <div className="card text-center" style={{ width: "18rem" }}>
+    <div className="card text-center " style={{ width: "18rem" }}>
       <div className="card-header">{header}</div>
-      <ul className="list-group list-group-flush">
-        {data.map((row) => {
-          if (cardType == "revenue") {
-            prefix = Object.keys(row);
-            row = Object.values(row);
-          } else {
-            const produtID = row.item_id;
-            const productAmount = row.amount;
-
-            row = `# ${produtID} , sold ${productAmount} times `;
-          }
-
-          return (
-            <li className="list-group-item">
-              {prefix} - {row} {suffix}
+      <ul className="list-group list-group-flush ">
+        {cardType == "singleLine" ? (
+          <li className="list-group-item">
+            {data} {suffix}
+          </li>
+        ) : cardType == "list" ? (
+          data.map((item, index) => (
+            <li className="list-group-item d-flex justify-content-between ">
+              <b>{index + 1} </b>
+              <div>{item.name}</div>
+              <div>{item.amount}</div>
             </li>
-          );
-        })}
+          ))
+        ) : cardType == "complex-singleLine" ? (
+          <>
+            <li className="list-group-item d-flex justify-content-between ">
+              <div>amount of deals : </div>
+              <div>{data.amount}</div>
+            </li>
+            <li className="list-group-item d-flex justify-content-between ">
+              <div>Money amount : </div>
+              <div>
+                {data.deals_amount} {suffix}
+              </div>
+            </li>
+            <li className="list-group-item d-flex justify-content-between ">
+              <div>Avrege for deal : </div>
+              <div>
+                {Math.round(data.deals_amount / data.amount)} {suffix}
+              </div>
+            </li>
+          </>
+        ) : (
+          ""
+        )}
       </ul>
     </div>
   );
--- pages/subjects/ZReport.jsx
+++ pages/subjects/ZReport.jsx
@@ -1,7 +1,105 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import StatsCard from "../../components/StatsCard";
+import {
+  getDealsDataInfo,
+  getSoldItems,
+  getTopSoldItems,
+  getTotalMoneyLast24Hours,
+} from "../service/statsService";
+import Link from "next/link";
+import styles from "../../styles/Home.module.css";
 
 const ZReport = () => {
-  return <div>dsfdsf</div>;
+  const [totalMoneyLast24Hours, setTotalMoneyLast24Hours] = useState();
+  const [topSoldItems, setTopSoldItems] = useState([]);
+  const [soldItems, setSoldItems] = useState([]);
+  const [cashdealsDataInfo, setCashDealsDataInfo] = useState({});
+  const [creditCardDealsDataInfo, setCreditCardDealsDataInfo] = useState({});
+
+  useEffect(async () => {
+    const { data } = await getTotalMoneyLast24Hours();
+    //console.log(data.result);
+    setTotalMoneyLast24Hours([data.result]);
+  }, []);
+
+  useEffect(async () => {
+    const { data } = await getTopSoldItems();
+    console.log(typeof data);
+    setTopSoldItems([...data.result]);
+  }, []);
+
+  useEffect(async () => {
+    const { data } = await getSoldItems();
+    setSoldItems([...data.result]);
+  }, []);
+
+  useEffect(async () => {
+    const { data } = await getDealsDataInfo();
+    setCashDealsDataInfo({
+      name: "cash",
+      amount: data.result.cash[0].pay_With_Cash,
+      deals_amount: data.result.cash[0].amount,
+    });
+    setCreditCardDealsDataInfo({
+      name: "credit card ",
+      amount: data.result.creditCard[0].pay_With_CreditCard,
+      deals_amount: data.result.creditCard[0].amount,
+    });
+  }, []);
+
+  return (
+    <div className="d-flex flex-column justify-content-around m-5">
+      <div className="d-flex justify-content-between ">
+        {/* <button className="m-4 btn btn-primary" onClick={}>home </button> */}
+
+        <Link href={`/`}>
+          <a className="m-4 btn btn-primary">home</a>
+        </Link>
+        <h2 className={styles.title}>Stats</h2>
+        <div></div>
+      </div>
+      <main className="d-flex  justify-content-around m-5">
+        <StatsCard
+          header="Top sold item"
+          prefix=""
+          data={topSoldItems}
+          suffix=""
+          cardType="list"
+        />
+        <div className="d-flex flex-column gap-5">
+          <StatsCard
+            header="Total deal amount last 24H"
+            prefix=""
+            data={totalMoneyLast24Hours}
+            suffix="$"
+            cardType="singleLine"
+          />
+          <StatsCard
+            header={`Total ${cashdealsDataInfo.name} deal amount `}
+            prefix=""
+            data={cashdealsDataInfo}
+            suffix="$"
+            cardType="complex-singleLine"
+          />
+
+          <StatsCard
+            header={`Total ${cashdealsDataInfo.name} deal amount `}
+            prefix=""
+            data={creditCardDealsDataInfo}
+            suffix="$"
+            cardType="complex-singleLine"
+          />
+        </div>
+        <StatsCard
+          header="Sold items "
+          prefix=""
+          data={soldItems}
+          suffix=""
+          cardType="list"
+        />
+      </main>
+    </div>
+  );
 };
 
 export default ZReport;
